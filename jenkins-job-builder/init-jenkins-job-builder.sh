a281a24fe99c9a753d9805bce38f261f5dfcb5eb
root <root@debian.de>
2018-10-15 18:16:49 +0200

    job builder and master are waiting endlessly

--- jenkins-job-builder/init-jenkins-job-builder.sh
+++ jenkins-job-builder/init-jenkins-job-builder.sh
@@ -4,4 +4,9 @@ git clone "https://git-inf.technikum-wien.at/ueb-inf/$FACH_FULL"
 
 cd "$FACH_FULL/jenkins-jobs"
 
+#while [ 400 != $(curl --write-out %{http_code} --silent --output /dev/null jenkins-master:8080) ]
+#do
+#	echo "nope"
+#done
+
 jenkins-jobs --conf /jenkins-job-builder/etc/jenkins-job-builder-conf.ini update *
