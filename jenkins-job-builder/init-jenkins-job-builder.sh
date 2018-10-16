654b46c32fd3283b8875e10a684e6616639a3718
root <root@debian.de>
2018-10-16 17:18:19 +0200

    using background process to prevent deadlock

--- jenkins-job-builder/init-jenkins-job-builder.sh
+++ jenkins-job-builder/init-jenkins-job-builder.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
+sed -i 's|$FACH_FULL|'"$FACH_FULL"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
+
 git clone "https://git-inf.technikum-wien.at/ueb-inf/$FACH_FULL"
 
 cd "$FACH_FULL/jenkins-jobs"
 
-#while [ 400 != $(curl --write-out %{http_code} --silent --output /dev/null jenkins-master:8080) ]
-#do
-#	echo "nope"
-#done
+(
+	while [ 302 != $(curl --write-out %{http_code} --silent --output /dev/null jenkins-master:8080/$FACH_FULL) ]
+	do
+		sleep 5
+	done
 
-jenkins-jobs --conf /jenkins-job-builder/etc/jenkins-job-builder-conf.ini update *
+	jenkins-jobs --conf /jenkins-job-builder/etc/jenkins-job-builder-conf.ini update *
+) &
