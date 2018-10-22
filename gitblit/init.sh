e294574b6247557cb13a685d51a6009430db3a21
kirchhof <kirchhof@technikum-wien.at>
2018-10-22 17:27:28 +0200

    cleaning up

--- gitblit/init.sh
+++ gitblit/init.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+cp /run/secrets/gitblit-public-key /opt/gitblit-data/ssh/gitblit.keys
+
 echo "server.contextPath = /$NGINX_PROXY_SUBDIRECTORY" >> /opt/gitblit-data-initial/gitblit.properties
+
 /run.sh
