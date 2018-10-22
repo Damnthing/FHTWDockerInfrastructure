f189eade0808772420d3e994b5a02163b2da11bd
kirchhof <kirchhof@technikum-wien.at>
2018-10-22 16:56:07 +0200

    cleaning up

--- jenkins-master/init.sh
+++ jenkins-master/init.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
-mkdir /var/jenkins_home/.ssh
+mkdir -p "${JENKINS_HOME}/.ssh"
+
+cp /run/secrets/gitblit-private-key "${JENKINS_HOME}/.ssh/gitblit.key"
 
 gitblitKey=`ssh-keyscan -p 29418 gitblit`
 while [ -z "$gitblitKey" ]
 do
 	gitblitKey=`ssh-keyscan -p 29418 gitblit`
 done
-echo $gitblitKey >> /var/jenkins_home/.ssh/known_hosts
+echo $gitblitKey >> "${JENKINS_HOME}/.ssh/known_hosts"
 
 jenkinsSlaveKey=`ssh-keyscan jenkins-slave`
 while [ -z "$jenkinsSlaveKey" ]
 do
         jenkinsSlaveKey=`ssh-keyscan jenkins-slave`
 done
-echo $jenkinsSlaveKey >> /var/jenkins_home/.ssh/known_hosts
+echo $jenkinsSlaveKey >> "${JENKINS_HOME}/.ssh/known_hosts"
 
 jenkinsJobBuilderKey=`ssh-keyscan jenkins-job-builder`
 while [ -z "$jenkinsJobBuilderKey" ]
 do
         jenkinsJobBuilderKey=`ssh-keyscan jenkins-job-builder`
 done
-echo $jenkinsJobBuilderKey >> /var/jenkins_home/.ssh/known_hosts
+echo $jenkinsJobBuilderKey >> "${JENKINS_HOME}/.ssh/known_hosts"
 
 sh `/sbin/tini -- /usr/local/bin/jenkins.sh`
 
