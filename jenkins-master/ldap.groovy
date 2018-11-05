2382803c10c8e480853237eb1425eecebf7455fe
kirchhof <kirchhof@technikum-wien.at>
2018-11-05 13:59:10 +0100

    added ldap groovy

---
+++ jenkins-master/ldap.groovy
@@ -0,0 +1,44 @@
+import jenkins.model.*
+import hudson.security.*
+import org.jenkinsci.plugins.*
+
+String ldap_server = 'ldap.technikum-wien.at:389'
+String ldap_rootDN = 'dc=technikum-wien,dc=at'
+String ldap_userSearchBase = 'ou=People'
+String ldap_userSearch = 'uid={0}'
+String ldap_groupSearchBase = ''
+String managerDN = ''
+String managerPassword = '{AQAAABAAAAAQ5yEQuh/4XgviVu8SU0ARoaabXFRlyUMSTaAZUn+pt2I=}'
+boolean inhibitInferRootDN = false
+String ldap_disableMailAddressResolver = false
+String ldap_displayNameAttributeName = 'cn'
+String ldap_mailAddressAttributeName = 'mail'
+    
+SecurityRealm ldap_realm = new LDAPSecurityRealm(server, rootDN, userSearchBase, userSearch, groupSearchBase, managerDN, managerPassword, inhibitInferRootDN) 
+Jenkins.instance.setSecurityRealm(ldap_realm)
+Jenkins.instance.save()
+
+def ldapRealm = new LDAPSecurityRealm(
+	ldap_server, //String server
+	ldap_rootDN, //String rootDN
+	ldap_userSearchBase, //String userSearchBase
+	ldap_userSearch, //String userSearch
+	'', //String groupSearchBase
+	'', //String groupSearchFilter
+	null, //LDAPGroupMembershipStrategy groupMembershipStrategy
+	'', //String managerDN
+	Secret.fromString(ldap_managerPassword), //Secret managerPasswordSecret
+	ldap_inhibitInferRootDN, //boolean inhibitInferRootDN
+	ldap_disableMailAddressResolver, //boolean disableMailAddressResolver
+	null, //CacheConfiguration cache
+	null, //EnvironmentProperty[] environmentProperties
+	ldap_displayNameAttributeName, //String displayNameAttributeName
+	ldap_mailAddressAttributeName, //String mailAddressAttributeName
+	IdStrategy.CASE_INSENSITIVE, //IdStrategy userIdStrategy
+	IdStrategy.CASE_INSENSITIVE //IdStrategy groupIdStrategy >> defaults
+)
+
+instance.setSecurityRealm(ldapRealm)
+
+// Save the state
+instance.save()
