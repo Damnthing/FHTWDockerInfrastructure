d79402e8af1920a750b5d16b7aaedd452a736ad9
kirchhof <kirchhof@technikum-wien.at>
2018-11-05 14:35:17 +0100

    fixing ldap groovy

--- jenkins-master/ldap.groovy
+++ jenkins-master/ldap.groovy
@@ -2,17 +2,26 @@ import jenkins.model.*
 import hudson.security.*
 import org.jenkinsci.plugins.*
 
-def instance = Jenkins.getInstance()
+String ldap_server = 'ldap.technikum-wien.at:389'
+String ldap_rootDN = 'dc=technikum-wien,dc=at'
+String ldap_userSearchBase = 'ou=People'
+String ldap_userSearch = 'uid={0}'
+String ldap_groupSearchBase = ''
+String ldap_managerPassword = '{AQAAABAAAAAQ5yEQuh/4XgviVu8SU0ARoaabXFRlyUMSTaAZUn+pt2I=}'
+boolean ldap_inhibitInferRootDN = false
+boolean ldap_disableMailAddressResolver = false
+String ldap_displayNameAttributeName = 'cn'
+String ldap_mailAddressAttributeName = 'mail'
 
 def ldapRealm = new LDAPSecurityRealm(
 	ldap_server, //String server
 	ldap_rootDN, //String rootDN
 	ldap_userSearchBase, //String userSearchBase
 	ldap_userSearch, //String userSearch
-	'', //String groupSearchBase
-	'', //String groupSearchFilter
+	(String)null, //String groupSearchBase
+	(String)null, //String groupSearchFilter
 	null, //LDAPGroupMembershipStrategy groupMembershipStrategy
-	'', //String managerDN
+	(String)null, //String managerDN
 	Secret.fromString(ldap_managerPassword), //Secret managerPasswordSecret
 	ldap_inhibitInferRootDN, //boolean inhibitInferRootDN
 	ldap_disableMailAddressResolver, //boolean disableMailAddressResolver
