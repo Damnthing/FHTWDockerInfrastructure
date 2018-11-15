4e727d912348a23d7f72fcd9e0f1bbc31c3dc7e7
kirchhof <kirchhof@technikum-wien.at>
2018-11-15 22:11:10 +0100

    adjustments for job builder files

--- jenkins-job-builder/init-jenkins-job-builder.sh
+++ jenkins-job-builder/init-jenkins-job-builder.sh
@@ -4,14 +4,14 @@
 sed -i 's|$FACH_FULL|'"$FACH_FULL"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
 
 # clone all jobs
-git clone "https://git-inf.technikum-wien.at/ueb-inf/$NGINX_PROXY_SUBDIRECTORY"
+git clone "https://git-inf.technikum-wien.at/ueb-inf/$COURSE.git"
 
 # do nothing while jenkins-master is not availale
 cd "$FACH_FULL/jenkins-jobs"
 
 # non blocking background job
 (
-	while [ 200 != $(curl --write-out %{http_code} --silent --output /dev/null http://jenkins-master:8080/$NGINX_PROXY_SUBDIRECTORY/) ]
+	while [ 200 != $(curl --write-out %{http_code} --silent --output /dev/null http://jenkins-master:8080/$COURSE/) ]
 	do
 		sleep 5
 	done
