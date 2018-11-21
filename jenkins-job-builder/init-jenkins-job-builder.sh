92ae3c72b9fe05bfec88d0c3d14cf9f5388a8356
root <root@lisa.lisa>
2018-11-21 15:13:13 +0100

    jenkins-job-builder config

--- jenkins-job-builder/init-jenkins-job-builder.sh
+++ jenkins-job-builder/init-jenkins-job-builder.sh
@@ -4,14 +4,14 @@
 sed -i 's|$COURSE|'"$COURSE"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
 
 # clone all jobs
-git clone "ssh://git@jenkins-ueb-inf@git-inf.technikum-wien.at/ueb-inf/$COURSE.git"
+git clone "ssh://git@git-inf.technikum-wien.at/ueb-inf/$COURSE.git"
 
 # do nothing while jenkins-master is not availale
 cd "./$COURSE"
 
 # non blocking background job
 (
-	while [ 200 != $(curl --write-out %{http_code} --silent --output /dev/null http://jenkins-master:8080/$COURSE/) ]
+	while [ 200 != $(curl --write-out %{http_code} --silent --output /dev/null http://jenkins-master:8080/$NGINX_PROXY_SUBDIRECTORY/) ]
 	do
 		sleep 5
 	done
