232e8f348e8a98b121e14084aeb84f91cab4167b
kirchhof <kirchhof@technikum-wien.at>
2018-11-21 13:51:35 +0100

    git working

--- jenkins-job-builder/init-jenkins-job-builder.sh
+++ jenkins-job-builder/init-jenkins-job-builder.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # set the correct path of the jenkins-master
-sed -i 's|$FACH_FULL|'"$FACH_FULL"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
+sed -i 's|$COURSE|'"$COURSE"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
 
 # clone all jobs
-git clone "ssh://jenkins-ueb-inf@git-inf.technikum-wien.at/ueb-inf/$COURSE.git"
+git clone "ssh://git@jenkins-ueb-inf@git-inf.technikum-wien.at/ueb-inf/$COURSE.git"
 
 # do nothing while jenkins-master is not availale
-cd "./$COURSE.git"
+cd "./$COURSE"
 
 # non blocking background job
 (
