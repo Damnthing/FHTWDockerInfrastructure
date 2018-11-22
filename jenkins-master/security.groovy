62fa5fb0a762ec6dcaf828e415b387526b00b5f5
root <root@lisa.lisa>
2018-11-22 17:41:04 +0100

    jenkins only has a single authentication method

--- jenkins-master/security.groovy
+++ jenkins-master/security.groovy
@@ -4,16 +4,19 @@ import jenkins.model.*
 import hudson.security.*
 import jenkins.security.s2m.AdminWhitelistRule
 
-# get jenkins instance
+// get jenkins instance
 def instance = Jenkins.getInstance()
- 
+
+// get user and password from file 
 def user = new File("$JENKINS_USER_FILE").text.trim()
 def pass = new File("$JENKINS_PASSWORD_FILE").text.trim()
  
+// create new Account
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount(user, pass)
 instance.setSecurityRealm(hudsonRealm)
  
+// full control strategy for user
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
 instance.save()
