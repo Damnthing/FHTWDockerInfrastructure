2f7ba60481fc396359c7f8d926c648a74b8fb8c6
kirchhof <kirchhof@technikum-wien.at>
2018-11-23 10:01:25 +0100

    split slave files

--- jenkins-job-builder/init-jenkins-job-builder.sh
+++ jenkins-job-builder/init-jenkins-job-builder.sh
@@ -1,21 +1,61 @@
 #!/bin/bash
 
+# set environment variables in current shell
+export INTERNAL_GIT_USER=$(cat $INTERNAL_GIT_USER_FILE)
 export JENKINS_USER=$(cat $JENKINS_USER_FILE)
 export JENKINS_PASSWORD=$(cat $JENKINS_PASSWORD_FILE)
 
-# set the correct path of the jenkins-master
+# copy keys and config for user jenkins
+mkdir -p "${JENKINS_AGENT_HOME}/.ssh"
+cp /init/config "${JENKINS_AGENT_HOME}/.ssh/config"
+cp "${SSH_SLAVE_PUBLIC_KEY_FILE}" "${JENKINS_AGENT_HOME}/.ssh/authorized_keys"
+cp "${INTERNAL_GIT_PRIVATE_KEY_FILE}" "${JENKINS_AGENT_HOME}/.ssh/internal-git-private-key"
+cp "${EXTERNAL_GIT_PRIVATE_KEY_FILE}" "${JENKINS_AGENT_HOME}/.ssh/external-git-private-key"
+sed -i 's|$INTERNAL_GIT_USER|'"$INTERNAL_GIT_USER"'|g' "${JENKINS_AGENT_HOME}/.ssh/config"
+
+# set ownership of jenkins agent home to user jenkins
+chown -Rf jenkins:jenkins "${JENKINS_AGENT_HOME}/.ssh"
+chmod 0700 -R "${JENKINS_AGENT_HOME}/.ssh"
+
+# copy keys and config for user root
+mkdir -p "${HOME}/.ssh"
+cp /init/config "${HOME}/.ssh/config"
+cp "${EXTERNAL_GIT_PRIVATE_KEY_FILE}" "${HOME}/.ssh/external-git-private-key"
+chmod 700 "${HOME}/.ssh/external-git-private-key"
+
+# ensure variables passed to docker container are also exposed to ssh sessions
+env | grep _ >> /etc/environment
+
+# add internal git server to known_hosts file
+gitInternalSshKey=`ssh-keyscan -p 29418 gitblit`
+while [ -z "$gitInternalSshKey" ]
+do
+        gitInternalSshKey=`ssh-keyscan -p 29418 gitblit`
+done
+echo $gitInternalSshKey >> "${JENKINS_AGENT_HOME}/.ssh/known_hosts"
+echo $gitInternalSshKey >> "${HOME}/.ssh/known_hosts"
+
+# add external git server to known_hosts file
+gitExternalSshKey=`ssh-keyscan git-inf.technikum-wien.at`
+while [ -z "$gitExternalSshKey" ]
+do
+        gitExternalSshKey=`ssh-keyscan git-inf.technikum-wien.at`
+done
+echo $gitExternalSshKey >> "${JENKINS_AGENT_HOME}/.ssh/known_hosts"
+echo $gitExternalSshKey >> "${HOME}/.ssh/known_hosts"
+
+# set jenkins-job-builder configuration values
 sed -i 's|$JENKINS_SUBDIRECTORY|'"$JENKINS_SUBDIRECTORY"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
 sed -i 's|$JENKINS_USER|'"$JENKINS_USER"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
 sed -i 's|$JENKINS_PASSWORD|'"$JENKINS_PASSWORD"'|g' /jenkins-job-builder/etc/jenkins-job-builder-conf.ini
 
 # clone all jobs
 git clone "ssh://git@git-inf.technikum-wien.at/ueb-inf/$COURSE.git"
-
-# do nothing while jenkins-master is not availale
 cd "./$COURSE"
 
 # non blocking background job
 (
+	# do nothing while jenkins-master is not availale
 	while [ 200 != $(curl --write-out %{http_code} --silent --output /dev/null http://jenkins-master:8080/$JENKINS_SUBDIRECTORY/) ]
 	do
 		sleep 5
@@ -23,5 +63,10 @@ cd "./$COURSE"
 
 	# create jobs
 	jenkins-jobs --conf /jenkins-job-builder/etc/jenkins-job-builder-conf.ini update .
-	#jenkins-jobs --conf /jenkins-job-builder/etc/jenkins-job-builder-conf.ini update /jenkins-job-builder/tests/yamlparser/fixtures/templates002.yaml
 ) &
+
+# generate ssh keys for the server
+ssh-keygen -A
+
+# run ssh slave
+exec /usr/sbin/sshd -D -e "${@}"
--- jenkins-slave/init.sh
+++ jenkins-slave/init.sh
@@ -3,19 +3,21 @@
 # set environment variables in current shell
 export INTERNAL_GIT_USER=$(cat $INTERNAL_GIT_USER_FILE)
 
-# copy keys and config
+# copy keys and config for user jenkins
 mkdir -p "${JENKINS_AGENT_HOME}/.ssh"
-mkdir -p "${HOME}/.ssh"
 cp /init/config "${JENKINS_AGENT_HOME}/.ssh/config"
-cp /init/config "${HOME}/.ssh/config"
 cp "${SSH_SLAVE_PUBLIC_KEY_FILE}" "${JENKINS_AGENT_HOME}/.ssh/authorized_keys"
 cp "${INTERNAL_GIT_PRIVATE_KEY_FILE}" "${JENKINS_AGENT_HOME}/.ssh/internal-git-private-key"
 cp "${EXTERNAL_GIT_PRIVATE_KEY_FILE}" "${JENKINS_AGENT_HOME}/.ssh/external-git-private-key"
-cp "${EXTERNAL_GIT_PRIVATE_KEY_FILE}" "${HOME}/.ssh/external-git-private-key"
-chmod 400 "${HOME}/.ssh/external-git-private-key"
-
-# set internal git user to pull with
 sed -i 's|$INTERNAL_GIT_USER|'"$INTERNAL_GIT_USER"'|g' "${JENKINS_AGENT_HOME}/.ssh/config"
+
+# set ownership of jenkins agent home to user jenkins
+chown -Rf jenkins:jenkins "${JENKINS_AGENT_HOME}/.ssh"
+chmod 0700 -R "${JENKINS_AGENT_HOME}/.ssh"
+
+# create custom work directory and set ownership to user jenkins
+mkdir -p /workspace-custom
+chown -Rf jenkins:jenkins "/workspace-custom"
 
 # ensure variables passed to docker container are also exposed to ssh sessions
 env | grep _ >> /etc/environment
@@ -27,6 +29,7 @@ do
         gitInternalSshKey=`ssh-keyscan -p 29418 gitblit`
 done
 echo $gitInternalSshKey >> "${JENKINS_AGENT_HOME}/.ssh/known_hosts"
+echo $gitInternalSshKey >> "${HOME}/.ssh/known_hosts"
 
 # add external git server to known_hosts file
 gitExternalSshKey=`ssh-keyscan git-inf.technikum-wien.at`
@@ -37,19 +40,8 @@ done
 echo $gitExternalSshKey >> "${JENKINS_AGENT_HOME}/.ssh/known_hosts"
 echo $gitExternalSshKey >> "${HOME}/.ssh/known_hosts"
 
-# if this is a job-builder-slave, create its jobs
-[ -e /init/init-jenkins-job-builder.sh ] && /init/init-jenkins-job-builder.sh || :
-
 # generate ssh keys for the server
 ssh-keygen -A
-
-# set ownership to user jenkins
-chown -Rf jenkins:jenkins "${JENKINS_AGENT_HOME}/.ssh"
-chmod 0700 -R "${JENKINS_AGENT_HOME}/.ssh"
-
-# create custom work directory and set ownership to user jenkins
-mkdir -p /workspace-custom
-chown -Rf jenkins:jenkins "/workspace-custom"
 
 # run ssh slave
 exec /usr/sbin/sshd -D -e "${@}"
