ab50db2b009e97822647544dcf9b2d8ea9f85d9e
kirchhof <kirchhof@technikum-wien.at>
2018-11-25 00:15:46 +0100

    permissions ..

--- jenkins-master/init.sh
+++ jenkins-master/init.sh
@@ -7,7 +7,7 @@ chown -Rf jenkins:jenkins "${JENKINS_HOME}/.ssh"
 # copy keys and config and set permissions
 cp /init/config "${JENKINS_HOME}/.ssh/config"
 cp "${INTERNAL_GIT_PRIVATE_KEY_FILE}" "${JENKINS_HOME}/.ssh/internal-git-private-key"
-chmod 0750 "${JENKINS_HOME}/.ssh/config"
+chmod 0740 "${JENKINS_HOME}/.ssh/config"
 chmod 0700 "${JENKINS_HOME}/.ssh/internal-git-private-key"
 
 # set variable values
