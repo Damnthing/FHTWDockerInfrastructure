a24842759c99a7246db68313e1689abd4eb1c291
kirchhof <kirchhof@technikum-wien.at>
2018-12-06 17:21:40 +0100

    join networks

--- start-nginx-proxy.sh
+++ start-nginx-proxy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --name nginx-proxy --network gpr1_course-network nginx-proxy:dev
+docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --name nginx-proxy nginx-proxy:dev
+
+docker network connect ws18-gpr1_course-network nginx-proxy
