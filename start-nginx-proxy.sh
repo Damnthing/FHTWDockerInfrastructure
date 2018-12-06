c726e6fe0557184abbf92a4a89f2fa361ca3a97a
root <root@lisa.lisa>
2018-12-06 17:49:13 +0100

    join network script

--- start-nginx-proxy.sh
+++ start-nginx-proxy.sh
@@ -2,4 +2,5 @@
 
 docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --name nginx-proxy nginx-proxy:dev
 
-docker network connect ws18-gpr1_course-network nginx-proxy
+docker network connect ws18-gpr1_course-network $(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $(docker service ps -q nginx-proxy))
+
