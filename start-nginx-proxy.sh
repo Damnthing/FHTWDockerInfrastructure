6ae57c41d664fec65dafd39b3d20257659664dbc
root <root@lisa.lisa>
2018-12-06 18:49:07 +0100

    trailing slash works

--- start-nginx-proxy.sh
+++ start-nginx-proxy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --name nginx-proxy nginx-proxy:dev
+#docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --name nginx-proxy nginx-proxy:dev
 
-docker network connect ws18-gpr1_course-network $(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $(docker service ps -q nginx-proxy))
+#docker network connect ws18-gpr1_course-network $(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $(docker service ps -q nginx-proxy))
+
+docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --network=ws18-gpr1_course-network --name nginx-proxy nginx-proxy:dev
 
