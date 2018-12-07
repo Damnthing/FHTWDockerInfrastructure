5d0049c5c9ebda1d60d4eddcb99244876f806778
root <root@lisa.lisa>
2018-12-07 12:15:14 +0100

    gpr2 compose stack added, started attach networks script

--- start-nginx-proxy.sh
+++ start-nginx-proxy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-#docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --name nginx-proxy nginx-proxy:dev
+docker network create -d overlay nginx-proxy-network
+docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --name nginx-proxy --network=nginx-proxy-network nginx-proxy:dev
 
-#docker network connect ws18-gpr1_course-network $(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $(docker service ps -q nginx-proxy))
+docker network connect ws18-gpr1_course-network $(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $(docker service ps -q nginx-proxy))
+docker network connect ws18-gpr2_course-network $(docker inspect --format '{{.Status.ContainerStatus.ContainerID}}' $(docker service ps -q nginx-proxy))
 
-docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --network=ws18-gpr1_course-network --name nginx-proxy nginx-proxy:dev
+#docker service create -p 80:80 --mount type=bind,source=/var/run/docker.sock,destination=/tmp/docker.sock --network=ws18-gpr1_course-network --name nginx-proxy nginx-proxy:dev
 
